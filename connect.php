bd43c2090e658641b64195c1e9fd2f2adf8c481e
Popzaii <ayes.tnputsr@gmail.com>
2026-03-17 02:05:33 +0700

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,17 @@
+<?php
+$servername = "localhost";
+$username = "root"; 
+$password = "";     
+$dbname = "ex05_613480111"; 
+
+// สร้างการเชื่อมต่อ
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// ตรวจสอบการเชื่อมต่อ
+if ($conn->connect_error) {
+    die("การเชื่อมต่อล้มเหลว: " . $conn->connect_error);
+}
+
+// ตั้งค่าให้รองรับภาษาไทย
+$conn->set_charset("utf8");
+?>
